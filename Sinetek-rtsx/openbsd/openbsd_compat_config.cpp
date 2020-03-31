964e4bc734cb1247783b693488ce99fa5c82bfe6
cholonam <cholonam@gmail.com>
2020-03-31 09:58:51 +0900

    Initialize and check dv_xname and call ca_attach/ca_detach

--- Sinetek-rtsx/openbsd/openbsd_compat_config.cpp
+++ Sinetek-rtsx/openbsd/openbsd_compat_config.cpp
@@ -1,5 +1,7 @@
 #include "openbsd_compat_config.h"
 
+#include <string.h> // strcmp
+
 #include "device.h"
 #include "sdmmcvar.h" // sdmmc_attach_args, sdmmc_softc_original
 
@@ -31,7 +33,11 @@ struct device *config_found_sm(struct device *parent, void *aux, cfprint_t print
 	struct sdmmc_softc_original *ret = UTL_MALLOC(struct sdmmc_softc_original);
 	bzero(ret, sizeof(struct sdmmc_softc_original));
 
+	// copy name (the only used member?)
+	strlcpy(ret->sc_dev.dv_xname, "sdmmc", sizeof(ret->sc_dev.dv_xname));
+
 	// call attach (and activate?)
+	UTL_CHK_PTR(sdmmc_ca.ca_attach, nullptr);
 	sdmmc_ca.ca_attach(parent, (struct device *) ret, aux);
 
 	return (struct device *) ret;
@@ -45,6 +51,16 @@ struct device *config_found_sm(struct device *parent, void *aux, cfprint_t print
 }
 
 int config_detach(struct device *dev, int flags) {
+	UTL_CHK_PTR(dev, 1);
+
+	if (strcmp(dev->dv_xname, "sdmmc") != 0) {
+		UTL_ERR("Device is not sdmmc!");
+		return 1;
+	}
+
+	// call detach
+	UTL_CHK_PTR(sdmmc_ca.ca_detach, 1);
+	sdmmc_ca.ca_detach(dev, flags);
 	// should free memory
 	return 1;
 }
