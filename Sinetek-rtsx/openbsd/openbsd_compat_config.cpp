d77c2a4ee7e23157ffbd665d250f6f6d6ce595aa
cholonam <cholonam@gmail.com>
2020-03-31 11:23:55 +0900

    Working on openbsd_compat_config.cpp (WIP)

--- Sinetek-rtsx/openbsd/openbsd_compat_config.cpp
+++ Sinetek-rtsx/openbsd/openbsd_compat_config.cpp
@@ -1,17 +1,26 @@
 #include "openbsd_compat_config.h"
 
+#include <libkern/OSMalloc.h> // OSMalloc, OSFree
 #include <string.h> // strcmp
 
 #include "device.h"
 #include "sdmmcvar.h" // sdmmc_attach_args, sdmmc_softc_original
 
 #define UTL_THIS_CLASS ""
-#include "util.h" // UTL_MALLOC, UTL_FREE
+#include "util.h" // UTL_CHK_PTR, UTL_ERR
 
 // See: https://github.com/openbsd/src/blob/master/sys/kern/subr_autoconf.c
 
 extern struct cfattach sdmmc_ca;
 extern struct cfdriver sdmmc_cd;
+extern struct cfdriver rtsx_cd;
+
+/// Array of cfdata to be matched
+/// TODO: should be this named cfdata as defined in device.h?
+static struct cfdata my_cfdata[2] = {
+	{ &sdmmc_ca, &sdmmc_cd },
+	//{ nullptr, &rtsx_cd },
+};
 
 #if 0 // not needed for now...
 // static for now...
@@ -20,28 +29,51 @@ static void *config_search(cfmatch_t fn, struct device *parent, void *aux)
 	void *ret = new sdmmc_softc_original;
 	return ret;
 }
+#endif
 
 /**
  * Attach a device (allocate memory for device)
  */
 static struct device *config_attach(struct device *parent, void *match, void *aux, cfprint_t print) {
-	return nullptr;
-}
-#endif
-
-struct device *config_found_sm(struct device *parent, void *aux, cfprint_t print, cfmatch_t submatch) {
-	struct sdmmc_softc_original *ret = UTL_MALLOC(struct sdmmc_softc_original);
-	bzero(ret, sizeof(struct sdmmc_softc_original));
+	struct device *ret;
+	struct cfdata *this_cfdata = (struct cfdata *) match;
+	int32_t devSize = (int32_t) this_cfdata->cf_attach->ca_devsize;
+	// allocate memory (TODO: check that it is freed on detach)
+	ret = (struct device *) OSMalloc(devSize, OSMT_DEFAULT);
+	UTL_CHK_PTR(ret, nullptr);
+	bzero(ret, devSize);
 
 	// copy name (the only used member?)
-	strlcpy(ret->sc_dev.dv_xname, "sdmmc", sizeof(ret->sc_dev.dv_xname));
+	UTL_CHK_PTR(this_cfdata->cf_driver, nullptr);
+	UTL_CHK_PTR(this_cfdata->cf_driver->cd_name, nullptr);
+
+	// set dv_xname (the only used attribute)
+	strlcpy(ret->dv_xname, this_cfdata->cf_driver->cd_name , sizeof(ret->dv_xname));
 
 	// call attach (and activate?)
-	UTL_CHK_PTR(sdmmc_ca.ca_attach, nullptr);
-	sdmmc_ca.ca_attach(parent, (struct device *) ret, aux);
+	UTL_CHK_PTR(this_cfdata->cf_attach->ca_attach, nullptr);
+	this_cfdata->cf_attach->ca_attach(parent, (struct device *) ret, aux);
 
 	return (struct device *) ret;
-	/*
+}
+
+/// This function is called from:
+///  - rtsx_attach, passing a struct sdmmcbus_attach_args as aux argument
+///  - sdmmc_io_attach, passing a struct sdmmc_attach_args as aux argument,
+///                             a print function, and
+///                             a submatch function (sdmmc_submatch)
+struct device *config_found_sm(struct device *parent, void *aux, cfprint_t print, cfmatch_t submatch)
+{
+	// check all members...
+	for (int i = 0; i < sizeof(my_cfdata) / sizeof(my_cfdata[0]); i++) {
+		if (!my_cfdata[i].cf_attach) continue;
+
+		if (my_cfdata[i].cf_attach->ca_match(parent, my_cfdata + i, aux)) {
+			return config_attach(parent, &my_cfdata[i], aux, print);
+		}
+	}
+	return nullptr; // not found
+	/* ORIGINAL OPENBSD CODE:
 	void *match = config_search(submatch, parent, aux);
 	if (match)
 		return config_attach(parent, match, aux, print);
@@ -50,7 +82,8 @@ struct device *config_found_sm(struct device *parent, void *aux, cfprint_t print
 	*/
 }
 
-int config_detach(struct device *dev, int flags) {
+int config_detach(struct device *dev, int flags)
+{
 	UTL_CHK_PTR(dev, 1);
 
 	if (strcmp(dev->dv_xname, "sdmmc") != 0) {
